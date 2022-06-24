10f3affcc2a11de26406c5f49505f7786184c4bb
Cedric Chee <cedric@invictusbyte.com>
2022-06-24 13:56:33 +0800

    disable the Rust standard lib

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+
+use core::panic::PanicInfo;
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
+
+fn main() {}
