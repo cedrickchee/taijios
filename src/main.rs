d069632a76e17bd34f0a6645ce98e3540266a7ef
Cedric Chee <cedric@invictusbyte.com>
2022-06-25 20:01:51 +0800

    chore: cleanup
    Remove outdated codes.

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,6 @@ use core::panic::PanicInfo;
 
 mod vga_buffer;
 
-// static HELLO: &[u8] = b"Hello World!";
-
 /// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -17,31 +15,6 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    /*
-    let vga_buffer = 0xb8000 as *mut u8; // cast the integer 0xb8000 into a raw pointer.
-    
-    // Iterate over the bytes of the static HELLO byte string.
-    for (i, &byte) in HELLO.iter().enumerate() {
-        // An unsafe block around all memory writes.
-        // 
-        // The reason is that the Rust compiler can’t prove that the raw
-        // pointers we create are valid. They could point anywhere and lead to
-        // data corruption. By putting them into an unsafe block we’re basically
-        // telling the compiler that we are absolutely sure that the operations
-        // are valid. Note that an unsafe block does not turn off Rust’s safety
-        // checks. It only allows you to do [five additional
-        // things](https://doc.rust-lang.org/stable/book/ch19-01-unsafe-rust.html#unsafe-superpowers).
-        unsafe {
-            // offset method calculates the offset from a pointer.
-            // 
-            // Use the offset method to write the string byte and the
-            // corresponding color byte (0xb is a light cyan).
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-    */
-
     // Write some characters to the screen.
     use core::fmt::Write;
     vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
