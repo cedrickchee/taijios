dedd049dcc4a3adf68b6ef3538aa53b0701f16a2
Cedric Chee <cedric@invictusbyte.com>
2022-06-27 13:17:31 +0800

    implement a custom test framework for our kernel
    Related: we still see our "Hello World" instead of the message from our
    `test_runner`. To fix this, we first need to change the name of the
    generated function to something different than `main` through
    the `reexport_test_harness_main` attribute.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
+#![feature(custom_test_frameworks)] // Rust supports replacing the default test framework through the unstable custom_test_frameworks feature
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"] // set the name of the test framework entry function to test_main
 
 use core::panic::PanicInfo;
 
@@ -22,5 +25,25 @@ pub extern "C" fn _start() -> ! {
     println!("Wörld!"); // test the handling of unprintable characters.
     println!("The numbers are {} and {}", 42, 1.0/3.0);
 
+    // Call the renamed test framework entry function.
+    #[cfg(test)] // use conditional compilation to add the call to `test_main` only in test contexts.
+    test_main();
+
     loop {}
+}
+
+/// Runner just prints a short debug message and then calls each test function
+/// in the list.
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    // ********** Sidenote **********
+    //
+    // syntax: &[&dyn Fn()] is a slice of trait object references of the Fn()
+    // trait. It is basically a list of references to types that can be called
+    // like a function.
+
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
 }
