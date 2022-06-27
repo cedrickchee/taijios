45403d664ed32255b95ad39a53e9c70e3482c3ef
Cedric Chee <cedric@invictusbyte.com>
2022-06-27 18:36:55 +0800

    configure test timeout duration
    Since `cargo test` waits until the test runner exits, a test that
    never returns can block the test runner forever.

--- src/main.rs
+++ src/main.rs
@@ -93,6 +93,6 @@ fn test_runner(tests: &[&dyn Fn()]) {
 #[test_case]
 fn trivial_assertion() {
     serial_print!("trivial assertion... ");
-    assert_eq!(0, 1);
+    assert_eq!(1, 1);
     serial_println!("[ok]");
 }
