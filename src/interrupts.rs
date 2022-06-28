757797782e54e59d9833b329392b49d43ba1a9ed
Cedric Chee <cedric@invictusbyte.com>
2022-06-28 18:32:39 +0800

    fix: compile error
    The error: "`idt` does not live long enough".
    
    In order to fix this problem, we need to store our `idt` at a place
    where it has a `'static` lifetime.

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -3,16 +3,40 @@
 //! Handle CPU exceptions in our kernel.
 
 use x86_64::structures::idt::{ InterruptDescriptorTable, InterruptStackFrame };
+use lazy_static::lazy_static;
 use crate::println;
 
-/// creates a new `InterruptDescriptorTable`.
+// Fix compile error: "`idt` does not live long enough".
+// 
+// In order to fix this problem, we need to store our `idt` at a place where it
+// has a `'static` lifetime. To achieve this we could allocate our IDT on the
+// heap using `Box` and then convert it to a `'static` reference, but we are
+// writing an OS kernel and thus don’t have a heap (yet).
+// 
+// As an alternative we could try to store the IDT as a `static`.
+//
+// However, there is a problem: Statics are immutable, so we can’t modify the
+// breakpoint entry from our `init` function. We could solve this problem by
+// using a `static mut`.
+//
+// This variant compiles without errors but it’s far from idiomatic. `static
+// mut`s are very prone to data races.
+//
+// Fortunately the `lazy_static` macro exists.
+lazy_static! {
+    /// creates a new `InterruptDescriptorTable`.
+    static ref IDT: InterruptDescriptorTable = {
+        let mut idt = InterruptDescriptorTable::new();
+        // Add breakpoint handler to our IDT.
+        idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt
+    };
+}
+
 pub fn init_idt() {
-    let mut idt = InterruptDescriptorTable::new();
-    // Add breakpoint handler to our IDT.
-    idt.breakpoint.set_handler_fn(breakpoint_handler);
     // In order that the CPU uses our new interrupt descriptor table, we need to
     // load it using the `lidt` instruction.
-    idt.load();
+    IDT.load();
 }
 
 /// A handler for the breakpoint exception.
