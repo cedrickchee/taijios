d147c8a9740feec772184dba9939798fe16cab8d
Cedric Chee <cedric@invictusbyte.com>
2022-07-07 17:07:10 +0800

    test the limitation of a bump allocator

--- tests/heap_allocation.rs
+++ tests/heap_allocation.rs
@@ -80,3 +80,41 @@ fn many_boxes() {
         assert_eq!(*x, i);
     }
 }
+
+// Like the `many_boxes` test, this test creates a large number of allocations
+// to provoke an out-of-memory failure if the allocator does not reuse freed
+// memory. Additionally, the test creates a `long_lived` allocation, which lives
+// for the whole loop execution.
+#[test_case]
+fn many_boxes_long_lived() {
+    let long_lived = Box::new(1); // test the limitation of a bump allocator
+    for i in 0..HEAP_SIZE {
+        let x = Box::new(i);
+        assert_eq!(*x, i);
+    }
+    assert_eq!(*long_lived, 1);
+}
+
+// ********** Sidenote **********
+//
+// # Bump Allocator
+//
+// ## The Drawback of a Bump Allocator
+// 
+// The main limitation of a bump allocator is that it can only reuse deallocated
+// memory after all allocations have been freed. This means that a single
+// long-lived allocation suffices to prevent memory reuse. We can see this when
+// we add a variation of the `many_boxes` test (`many_boxes_long_lived`).
+//
+// When we try run `many_boxes_long_lived` test, we see that it fails.
+//
+// Why this failure occurs in detail: First, the `long_lived` allocation is
+// created at the start of the heap, thereby increasing the `allocations` counter
+// by 1. For each iteration of the loop, a short lived allocation is created and
+// directly freed again before the next iteration starts. This means that the
+// `allocations` counter is temporarily increased to 2 at the beginning of an
+// iteration and decreased to 1 at the end of it. The problem now is that the
+// bump allocator can only reuse memory when all `allocations` have been freed,
+// i.e. the `allocations` counter falls to 0. Since this doesn’t happen before the
+// end of the loop, each loop iteration allocates a new region of memory,
+// leading to an out-of-memory error after a number of iterations.
